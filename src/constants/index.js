11c220eba5a2c33319bed348e1faccefcca0e54b
Anton Volyk <anton_volyk@epam.com>
2021-04-10 01:37:12 +0300

    implemented module1 task3

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+const EXIT_CODE = {
+  success: 0,
+  error: 1
+};
+const USER_ARGV_INDEX = 2;
+const DEFAULT_COMMAND = `--help`;
+
+module.exports = {
+  EXIT_CODE,
+  USER_ARGV_INDEX,
+  DEFAULT_COMMAND
+}
